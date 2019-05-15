b18be2fbdd0b2eb7c96f40890a784d485f41821c
Nattanon Huabnarin <nattanon.hu@gmail.com>
2019-05-16 03:33:03 +0700

    add LoadingGUI

--- src/constants/FontHolder.java
+++ src/constants/FontHolder.java
@@ -9,10 +9,12 @@ public class FontHolder {
 	private static final FontHolder instance = new FontHolder();
 	public static FontHolder getInstance () { return instance; }
 	
+	public Font font48;
 	public Font font36;
 	public Font font28;
 	
 	public FontHolder() {
+		font48 = loadFont("upheavtt", TTF, 48);
 		font36 = loadFont("upheavtt", TTF, 36);
 		font28 = loadFont("upheavtt", TTF, 28);
 	}
